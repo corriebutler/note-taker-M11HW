7351f2a99a1b45871606ecbd2a7cdd01dd9b5546
Corrie Butler <corriebutler12@gmail.com>
2021-12-03 15:44:11 -0700

    fixed file paths in html router

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -3,18 +3,18 @@ const router = require('express').Router();
 
 // Get the Index page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
 });
 
 // Get the Notes page
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../Develop/public/notes.html'));
 });
 
 
 // Catch all just in case
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
 });
 
 module.exports = router;
